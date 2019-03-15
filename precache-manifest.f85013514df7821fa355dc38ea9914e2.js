5bddd7978bc979206210f68d967697afbe078344
Glen <glenmccann2@gmail.com>
2019-03-15 09:42:43 -0400

    Updates

---
+++ precache-manifest.f85013514df7821fa355dc38ea9914e2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "cda8884f3c66bdb1e215a9568c4a2dc7",
+    "url": "/static/media/imports.cda8884f.png"
+  },
+  {
+    "revision": "14ce1f52964b25655875",
+    "url": "/static/css/main.119c5b49.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c6e27bfbc3c6225ede1cabb111ad4848",
+    "url": "/static/media/browsers.c6e27bfb.png"
+  },
+  {
+    "revision": "8c15f916387a297848fe",
+    "url": "/static/js/2.c278f327.chunk.js"
+  },
+  {
+    "revision": "14ce1f52964b25655875",
+    "url": "/static/js/main.d4dbeea8.chunk.js"
+  },
+  {
+    "revision": "8dadfa709275861326cd776624fe27af",
+    "url": "/static/media/props.8dadfa70.png"
+  },
+  {
+    "revision": "c06626d5dbbf1d96a5c651a6eb641472",
+    "url": "/static/media/basicFunctions.c06626d5.png"
+  },
+  {
+    "revision": "e5aa8208f84db39987a365c08523c49b",
+    "url": "/static/media/richFunctions.e5aa8208.png"
+  },
+  {
+    "revision": "8c15f916387a297848fe",
+    "url": "/static/css/2.78e29c77.chunk.css"
+  },
+  {
+    "revision": "2dc664c1debeb07b2ce0db6550221dac",
+    "url": "/index.html"
+  }
+];
